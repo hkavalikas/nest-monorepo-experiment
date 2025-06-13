f29ef3fcfa044c63f6dcfcf6007127e3284efc4a
Harry (Charalampos) Kavalikas <harry.kavalikas@hotmail.com>
2025-06-13 09:08:13 +0100

    fix: env variable support

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -11,6 +11,7 @@ export class AppController {
   @ApiOperation({ summary: 'Get a greeting message' })
   @ApiResponse({ status: 200, description: 'Returns the greeting message' })
   getHello(): string {
+    console.log(process.env.DATABASE_URL);
     return this.appService.getHello();
   }
 }
